a7074cf0d0b35b4a2a597afc814b096db3f8f1c4
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-08-21 14:09:32 -0400

    creación de varios componentes

---
+++ js/home/arcordeon.js
@@ -0,0 +1,14 @@
+// document.querySelectorAll(".accordion-header").forEach(header => {
+//   header.addEventListener("click", () => {
+//     const item = header.parentElement;
+//     const body = item.querySelector(".accordion-body");
+
+//     // Cerrar todos
+//     document.querySelectorAll(".accordion-body").forEach(b => b.classList.remove("show"));
+//     document.querySelectorAll(".accordion-header").forEach(h => h.classList.remove("active"));
+
+//     // Abrir el clicado
+//     header.classList.add("active");
+//     body.classList.add("show");
+//   });
+// });
