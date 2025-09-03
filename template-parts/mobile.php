97fb39a7a474b8dc2b4ec8f9485a0224ea602dfb
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-09-03 15:11:42 -0400

    mejorando el flujo visual de la seccion 12 mobile

--- template-parts/mobile.php
+++ template-parts/mobile.php
@@ -1,31 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/css/home/home.css">
-    <title>Document</title>
-</head>
-<body>
-    
-    <Section class="section-12" id="disponibilidad">
-        <span>El portafirmas estÁ disponible para dispositivos móviles en cualquier sistema operativo</h1>
-            <div class="div1-section-12">
-                <h1 class="">Disponibilidad móvil</h1>
-                <div class="div2-section-12">
-                    <img class="img" src="/img/smartmockups_lhkytam5 1.svg" alt="">
+<section class="section-12" id="disponibilidad">
+    <div class="orange-corner"></div>
+    <p class="section-text">EL PORTAFIRMAS ESTÁ DISPONIBLE PARA DISPOSITIVOS MÓVILES EN CUALQUIER SISTEMA OPERATIVO</p>
+    <h2>Disponibilidad móvil</h2>
 
+    <div class="devices-display">
+        <img src="/img/smartmockups_lhkytam5 1.svg" alt="Dispositivos Firma GOB">
+    </div>
 
-                    <div class="div3-section-12">
-                        <div class="div4-section-12">ANDROID</div>
-                        <div class="div5-section-12"><img src="/img/image 7.svg" alt=""></div>
-                    </div>
-                    <div class="div6-section-12">
-                        <div class="div7-section-12">APPLE</div>
-                        <div class="div8-section-12"><img src="/img/image 7.svg" alt=""></div>
-                    </div>
-                </div>
-    </Section>
-    
-</body>
+    <div class="downloads-container">
+        <div class="download-box android">
+            <span class="platform-name">ANDROID</span>
+            <div class="qr-code">
+                <img src="/img/image 7.svg" alt="QR Android">
+            </div>
+        </div>
+
+        <div class="download-box apple">
+            <span class="platform-name">APPLE</span>
+            <div class="qr-code">
+                <img src="/img/image 7.svg" alt="QR Apple">
+            </div>
+        </div>
+    </div>
+
+    <p class="join-text">¡Únete a Firma GOB hoy!</p>
+</section>
+
 </html>
