cb779769394457301486f9be19e9c430f5122e87
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-09-03 11:07:30 -0400

    cambios de nombre de la secciones y eliminacion de algunas funciones innesesarias

---
+++ template-parts/banner.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Template part for displaying section 1 of the home page
+ *
+ * @package Firma-gob-2025
+ */
+?>
+
+<section class="section-1">
+    <div class="div-1">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/9479378e080c060d5c155b7fc1381ac84d50a61b.png" alt="backImg">
+    </div>
+    <div class="div-2">
+        <img class="img-div-2" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/Vector 1.svg" alt="Vector decorativo">
+        <div class="div-2_div">
+            <h1>
+                En OGTIC
+                estamos comprometidos
+                para brindarte el mejor
+                servicio de plataforma <span style="color: #FFC000;">
+                    portafirmas
+                </span>
+            </h1>
+            <div class="logo-section1">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/firma_Mesa de trabajo 1 copia 6 3.svg" alt="Img Firma">
+            </div>
+        </div>
+    </div>
+</section>
