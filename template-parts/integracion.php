c5341cb237adba4406be587cdec8c1c0b9454f4c
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-09-05 14:48:23 -0400

    reparo de integracion y la paleta de colores

--- template-parts/integracion.php
+++ template-parts/integracion.php
@@ -5,7 +5,7 @@
     <article>
       <!-- Círculo/figura izquierda -->
       <div style="background-color: #A6D2FF63; display: flex; justify-content: center; align-items: center; height: 250px; width: 250px; border-radius: 50%;">
-        <div class="int-col int-left" style="border-radius: 50%; background-color: #0066CC; height: 200px; width: 200px; display: flex; justify-content: center; align-items: center;">
+        <div class="int-col int-left" style="border-radius: 50%; background-color:#5EAEFF; height: 200px; width: 200px; display: flex; justify-content: center; align-items: center;">
           <img class="int-figure int-figure--left"
                src="<?php echo get_template_directory_uri(); ?>/img/Ellipse 22.svg"
                alt="Persona izquierda" style="max-width: 90%; height: auto;">
@@ -23,7 +23,7 @@
     
 
     <!-- Segundo artículo -->
-    <article style=" backgroung-color:red; justify-content:end;">
+    <article style=" display: flex;  justify-content:end;">
       <!-- Tarjeta azul (contenido) -->
       <div class="bubble bubble--card">
         <h3>Capacidad de integración mediante<br> Sistema API</h3>
@@ -32,7 +32,7 @@
 
       <!-- Círculo/figura derecha -->
       <div style="background-color: #A6D2FF; border-radius: 50%; width: 300px; height: 300px; display: flex; justify-content: center; align-items: center;">
-        <div class="int-col int-right" style="background-color: #0066CC; border-radius: 50%; width: 250px; height: 250px;  ">
+        <div class="int-col int-right" style="background-color:#5EAEFF; border-radius: 50%; width: 250px; height: 250px;  ">
           <img class="int-figure int-figure--right"
                src="<?php echo get_template_directory_uri(); ?>/img/Ellipse 21.svg"
                alt="Persona derecha" style="max-width: 100%; height: auto;">
