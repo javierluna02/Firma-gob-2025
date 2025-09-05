0f8b75f611489a64f56a2c604e6bebca53c262b6
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-09-05 15:09:35 -0400

    agrego de brillitos

--- template-parts/integracion.php
+++ template-parts/integracion.php
@@ -1,10 +1,15 @@
+ <img src="<?php echo get_template_directory_uri(); ?>/img/Group 27.svg" alt="">
+
+
 <section class="sec-integracion" id="integracion">
   <div class="int-wrap">
 
     <!-- Primer artículo -->
+     
     <article>
       <!-- Círculo/figura izquierda -->
       <div style="background-color: #A6D2FF63; display: flex; justify-content: center; align-items: center; height: 250px; width: 250px; border-radius: 50%;">
+        
         <div class="int-col int-left" style="border-radius: 50%; background-color:#5EAEFF; height: 200px; width: 200px; display: flex; justify-content: center; align-items: center;">
           <img class="int-figure int-figure--left"
                src="<?php echo get_template_directory_uri(); ?>/img/Ellipse 22.svg"
@@ -31,14 +36,18 @@
       </div>
 
       <!-- Círculo/figura derecha -->
+        <img src="<?php echo get_template_directory_uri(); ?>/img/Group 27.svg" alt="">
+
       <div style="background-color: #A6D2FF; border-radius: 50%; width: 300px; height: 300px; display: flex; justify-content: center; align-items: center;">
         <div class="int-col int-right" style="background-color:#5EAEFF; border-radius: 50%; width: 250px; height: 250px;  ">
+          
           <img class="int-figure int-figure--right"
                src="<?php echo get_template_directory_uri(); ?>/img/Ellipse 21.svg"
                alt="Persona derecha" style="max-width: 100%; height: auto;">
+               
         </div>
       </div>
-    </article>
-
+  
+    </article>   
   </div>
 </section>
