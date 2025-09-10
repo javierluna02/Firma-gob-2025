ba2e3c20ec3d84ddadf3a0498d1d070c5d3817d3
Cesar Terrero <cesarbolivar.bt@gmail.com>
2025-09-10 09:01:41 -0400

    Refines banner layout and removes unused images
    Updates banner styling for improved visual hierarchy, increasing heading width and adjusting its position. Removes decorative and signature images from markup to streamline design and reduce clutter.

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -12,16 +12,14 @@
         <img src="<?php echo get_field('imagen_banner', 'option')['url']; ?>" alt="backImg">
     </div>
     <div class="div-2">
-        <img class="img-div-2" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/Vector 1.svg" alt="Vector decorativo">
+
         <div class="div-2_div">
             <h1>
                 <?php echo get_field('texto_principal_banner', 'option'); ?><span style="color: <?php echo get_field('color_del_resaltado', 'option'); ?>;">
                   <?php echo get_field('texto_resaltado_banner', 'option'); ?>
                 </span>
             </h1>
-            <div class="logo-section1">
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/firma_Mesa de trabajo 1 copia 6 3.svg" alt="Img Firma">
-            </div>
+       
         </div>
     </div>
 </section>
