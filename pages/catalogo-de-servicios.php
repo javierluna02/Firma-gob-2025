08ccb08d63a190ee5c5fd099d26da7a0e38056cc
masterk319 <nelsonjavierhernandez016@gmail.com>
2025-09-25 09:24:15 -0400

    correccion de algunos detalles

--- pages/catalogo-de-servicios.php
+++ pages/catalogo-de-servicios.php
@@ -10,7 +10,7 @@
     <div class="div-1">
       <img class="chico" src="<?= get_template_directory_uri(); ?>/img/Group_108_(2)[1].png" alt="">
     </div>
-    <div class="div-2">
+    <div class="div-2"> 
 
       <div class="div-2_div">
         <h1>
--- pages/detalles-de-servicio.php
+++ pages/detalles-de-servicio.php
@@ -42,8 +42,8 @@ get_template_part('template-parts/header');
     <p style="width: 80%;">Consiste en proveer a los organismos gubernamentales de un portafirmas altamente efectivo para administrar de manera eficiente el flujo de documentos que requieren ser firmados digitalmente. Esta solución tecnológica permite a las instituciones públicas agilizar y automatizar el proceso de firma, reduciendo la dependencia del papel y optimizando la gestión documental.</p>
 </div>
 
-<section style=" display:flex; ">
-    <div style=" display: flex ; border: 1px solid #0000; flex-direction:column;  width: 50%; ">
+<section style=" display:flex; justify-content: center; align-items: center; ">
+    <div style=" display: flex ; padding:20px; border: 1px solid #0000; flex-direction:column;  width: 50%; ">
         <div style="width:196px;   font-weight: 600; color: white; display:flex; align-items:center; justify-content: center; height: 47px; background-color: #7CC713; border-radius: 23px ;   ">
             Beneficios
         </div>
@@ -68,7 +68,7 @@ get_template_part('template-parts/header');
     </div>
 
 
-    <div style=" padding:30px; width:50%;">
+    <div style=" padding:30px; width:40%;">
 
         <div style=" padding: 20px; width: 520px; border:1px solid #000;
          height: 340px ; display: flex;
@@ -98,6 +98,67 @@ get_template_part('template-parts/header');
 
 
 </section>
+
+<section style=" display:flex; justify-content: center; align-items: center; ">
+    <div style=" display: flex ; border: 1px solid #0000; flex-direction:column;  width: 50%; ">
+        <div style="width:196px;   font-weight: 600; color: white; display:flex; align-items:center; justify-content: center; height: 47px; background-color: #7CC713; border-radius: 23px ;   ">
+            Beneficios
+        </div>
+
+        <div style="display: flex; justify-content: center;">
+            <img src="" alt="">
+            <p>Flujos de Firma: Creación de flujos de firmas en cascada, simultáneos y vistos buenos. </p>
+        </div>
+
+        <div style="display: flex; justify-content: center;">
+            <img src="" alt="">
+            <p>Firma desde cualquier lugar: Plataforma adaptable a dispositivos móviles, permitiendo firmar desde donde se encuentre. </p>
+        </div>
+
+        <div style="display: flex; justify-content: center;">
+            <img src="" alt="">
+            <p>Seguridad e Integridad: Los documentos firmados no pueden ser modificados, por lo que se garantiza la autenticidad de este. </p>
+        </div>
+
+
+
+    </div>
+
+
+    <div style=" padding:30px; width:40%;">
+
+        <div style=" padding: 20px; width: 520px; border:1px solid #000;
+         height: 340px ; display: flex;
+         flex-direction: column; justify-content: center; ">
+            <div style="display: flex; align-items: center; justify-content: center; ">
+                <img style=" width:20px; border:  1px solid #000000" src="" alt="">
+                <div>
+                    <h4>
+                        Información adicional
+                    </h4>
+                    <ol>
+                        <li>Para tramitar este servicio, es necesario que el solicitante haya enviado la carta de solicitud firmada, sellada e inicialada por la máxima autoridad.</li>
+                    </ol>
+                </div>
+
+            </div>
+
+            <ul>
+                Toda documentación adjuntada en el sistema será almacenada en los servidores de la OGTIC (Data Center)
+            </ul>
+
+            <a href="">conocer mas</a>
+        </div>
+
+    </div>
+
+
+
+</section>
+<div style=" display: flex; justify-content: center; align-items: center;  height: 20vh; ">
+    <button style="width: 198px; height: 43px
+    ; ">Servicios</button>
+</div>
 
 <?php
 get_template_part('template-parts/footer');
